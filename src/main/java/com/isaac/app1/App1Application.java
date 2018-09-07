a4e13dacb9183d19550a7ea416fd1804dff89429
Isaac Ramon <ramonduartekk@gmail.com>
2018-09-07 20:21:08 -0300

    Criando Operação de Instancia

--- src/main/java/com/isaac/app1/App1Application.java
+++ src/main/java/com/isaac/app1/App1Application.java
@@ -1,12 +1,31 @@
 package com.isaac.app1;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.isaac.app1.repository.categoriaRepository;
+import com.isaac.app1.resources.domain.Categoria;
+
 @SpringBootApplication
-public class App1Application {
+public class App1Application implements CommandLineRunner {
+	
+	@Autowired
+	private categoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null,"Escritorio");
+		Categoria cat2 = new Categoria(null, "Informatica");
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 }
