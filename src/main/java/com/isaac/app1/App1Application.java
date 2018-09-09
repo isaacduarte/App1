73ef8b2682561f4bef76d3ba7d826765d3d3e3bf
Isaac Ramon <ramonduartekk@gmail.com>
2018-09-09 13:21:07 -0300

    Cidade, Estado

--- src/main/java/com/isaac/app1/App1Application.java
+++ src/main/java/com/isaac/app1/App1Application.java
@@ -8,9 +8,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.isaac.app1.repository.CidadeRepository;
+import com.isaac.app1.repository.EstadoRepository;
 import com.isaac.app1.repository.categoriaRepository;
 import com.isaac.app1.repository.produtoRepository;
 import com.isaac.app1.resources.domain.Categoria;
+import com.isaac.app1.resources.domain.Cidade;
+import com.isaac.app1.resources.domain.Estado;
 import com.isaac.app1.resources.domain.Produto;
 
 @SpringBootApplication
@@ -21,6 +25,12 @@ public class App1Application implements CommandLineRunner {
 	
 	@Autowired
 	private produtoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App1Application.class, args);
@@ -46,6 +56,20 @@ public class App1Application implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Ceara");
+		Estado est2 = new Estado(null, "Paraiba");
+		
+		Cidade c1 = new Cidade(null, "Fortaleza", est1);
+		Cidade c2 = new Cidade(null, "Cajazeiras", est2);
+		Cidade c3 = new Cidade(null, "Ipaumirim", est1);
+		 
+		est1.getCidades().addAll(Arrays.asList(c1, c3));
+		est2.getCidades().addAll(Arrays.asList(c2));
+		
+		
+		 estadoRepository.saveAll(Arrays.asList(est1, est2));
+		 cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	
 		
 	}
